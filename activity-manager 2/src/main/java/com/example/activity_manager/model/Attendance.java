74b43f6b92477a2b2fe76eaac983425a6204b5c1
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2025-12-15 15:20:52 +0200

    Add attendance entity and class for composite PK
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ activity-manager 2/src/main/java/com/example/activity_manager/model/Attendance.java
@@ -0,0 +1,30 @@
+/** Clasa pentru prezenta studentilor
+ * @author Stroiu Alexandra-Ioana
+ * @version 15 Decembrie 2025
+ */
+package com.example.activity_manager.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Attendance")
+public class Attendance {
+    @EmbeddedId
+    private AttendanceId id; //TODO
+
+    @ManyToOne
+    @MapsId("sessionId")
+    @JoinColumn(name = "session_id")
+    private CourseSession session;
+
+    @ManyToOne
+    @MapsId("studentId")
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "attendance_status", nullable = false)
+    private AttendanceStatus attendanceStatus = AttendanceStatus.ABSENT;
+
+    // getters & setters
+}
---
+++ activity-manager 2/src/main/java/com/example/activity_manager/model/AttendanceId.java
@@ -0,0 +1,10 @@
+package com.example.activity_manager.model;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+//TODO
+@Embeddable
+public class AttendanceId implements Serializable {
+}
