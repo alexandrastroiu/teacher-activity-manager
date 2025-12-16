ad800b40532902ec033bc0aad802cf16a8cf5b07
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2025-12-16 16:44:10 +0200

    Modify DB schema to work with JPA
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- database/schema.sql
+++ database/schema.sql
@@ -3,54 +3,54 @@ CREATE DATABASE IF NOT EXISTS activity_manager CHARACTER SET utf8mb4 COLLATE utf
 USE activity_manager;
 -- Create table: Users
 CREATE TABLE Users (
-  user_id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id BIGINT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(50) NOT NULL UNIQUE,
   user_password VARCHAR(255) NOT NULL,
-  user_role ENUM('student', 'teacher', 'admin') NOT NULL DEFAULT 'student',
+  user_role ENUM('STUDENT', 'TEACHER', 'ADMIN') NOT NULL DEFAULT 'STUDENT',
   user_email VARCHAR(50) UNIQUE
 );
 -- Create table: Departments
 CREATE TABLE Departments (
-  department_id INT AUTO_INCREMENT PRIMARY KEY,
+  department_id BIGINT AUTO_INCREMENT PRIMARY KEY,
   department_name VARCHAR(50) NOT NULL UNIQUE
 );
 -- Create table: Student_Groups
 CREATE TABLE Student_Groups (
-  group_id INT AUTO_INCREMENT PRIMARY KEY,
+  group_id BIGINT AUTO_INCREMENT PRIMARY KEY,
   group_name VARCHAR(50) NOT NULL UNIQUE
 );
 -- Create table: Teachers
 CREATE TABLE Teachers (
-  teacher_id INT AUTO_INCREMENT PRIMARY KEY,
+  teacher_id BIGINT AUTO_INCREMENT PRIMARY KEY,
   last_name VARCHAR(50) NOT NULL,
   first_name VARCHAR(50) NOT NULL,
-  department_id INT NOT NULL,
-  user_id INT NOT NULL UNIQUE,
+  department_id BIGINT NOT NULL,
+  user_id BIGINT NOT NULL UNIQUE,
   FOREIGN KEY (department_id) REFERENCES Departments(department_id),
   FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 -- Create table: Students
 CREATE TABLE Students (
-  student_id INT AUTO_INCREMENT PRIMARY KEY,
+  student_id BIGINT AUTO_INCREMENT PRIMARY KEY,
   last_name VARCHAR(50) NOT NULL,
   first_name VARCHAR(50) NOT NULL,
-  group_id INT NOT NULL,
-  user_id INT NOT NULL UNIQUE,
+  group_id BIGINT NOT NULL,
+  user_id BIGINT NOT NULL UNIQUE,
   FOREIGN KEY (group_id) REFERENCES Student_Groups(group_id),
   FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 -- Create table: Courses
 CREATE TABLE Courses (
-  course_id INT AUTO_INCREMENT PRIMARY KEY,
+  course_id BIGINT AUTO_INCREMENT PRIMARY KEY,
   course_name VARCHAR(50) NOT NULL UNIQUE,
-  course_type ENUM('mandatory', 'optional', 'elective') DEFAULT 'mandatory',
-  teacher_id INT NOT NULL,
+  course_type ENUM('MANDATORY', 'OPTIONAL', 'ELECTIVE') DEFAULT 'MANDATORY',
+  teacher_id BIGINT NOT NULL,
   FOREIGN KEY (teacher_id) REFERENCES Teachers(teacher_id)
 );
 -- Create table: Course_Sessions
 CREATE TABLE Course_Sessions (
-  session_id INT AUTO_INCREMENT PRIMARY KEY,
-  course_id INT NOT NULL,
+  session_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  course_id BIGINT NOT NULL,
   session_date DATE NOT NULL,
   session_time TIME NOT NULL,
   duration TIME DEFAULT '02:00:00',
@@ -58,45 +58,45 @@ CREATE TABLE Course_Sessions (
 );
 -- Create table: Course_Enrollment
 CREATE TABLE Course_Enrollment (
-  student_id INT NOT NULL,
-  course_id INT NOT NULL,
-  enrollment_type ENUM('active', 'repetition') DEFAULT 'active',
+  student_id BIGINT NOT NULL,
+  course_id BIGINT NOT NULL,
+  enrollment_type ENUM('ACTIVE', 'REPETITION') DEFAULT 'ACTIVE',
   PRIMARY KEY (student_id, course_id),
   FOREIGN KEY (student_id) REFERENCES Students(student_id),
   FOREIGN KEY (course_id) REFERENCES Courses(course_id)
 );
 -- Create table: Attendance
 CREATE TABLE Attendance (
-  session_id INT NOT NULL,
-  student_id INT NOT NULL,
-  attendance_status ENUM('present', 'absent', 'excused') DEFAULT 'absent',
+  session_id BIGINT NOT NULL,
+  student_id BIGINT NOT NULL,
+  attendance_status ENUM('PRESENT', 'ABSENT', 'EXCUSED') DEFAULT 'ABSENT',
   PRIMARY KEY (session_id, student_id),
   FOREIGN KEY (session_id) REFERENCES Course_Sessions(session_id),
   FOREIGN KEY (student_id) REFERENCES Students(student_id)
 );
 -- Create table: Activities
 CREATE TABLE Activities (
-  activity_id INT AUTO_INCREMENT PRIMARY KEY,
-  teacher_id INT NOT NULL,
+  activity_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  teacher_id BIGINT NOT NULL,
   title VARCHAR(100) NOT NULL,
   description TEXT,
   start_date DATE,
   end_date DATE NOT NULL,
   status ENUM(
-    'in progress',
-    'suspended',
-    'resumed',
-    'repeated',
-    'completed'
-  ) NOT NULL DEFAULT 'in progress',
-  priority ENUM('low', 'medium', 'high', 'urgent') NOT NULL DEFAULT 'low',
-  difficulty ENUM('easy', 'medium', 'hard') NOT NULL DEFAULT 'easy',
+    'IN_PROGRESS',
+    'SUSPENDED',
+    'RESUMED',
+    'REPEATED',
+    'COMPLETED'
+  ) NOT NULL DEFAULT 'IN_PROGRESS',
+  priority ENUM('LOW', 'MEDIUM', 'HIGH', 'URGENT') NOT NULL DEFAULT 'LOW',
+  difficulty ENUM('EASY', 'MEDIUM', 'HARD') NOT NULL DEFAULT 'EASY',
   FOREIGN KEY (teacher_id) REFERENCES Teachers(teacher_id)
 );
 -- Create table Activity_Subtask
 CREATE TABLE Activity_Subtasks (
-  subtask_id INT AUTO_INCREMENT PRIMARY KEY,
-  activity_id INT NOT NULL,
+  subtask_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  activity_id BIGINT NOT NULL,
   title VARCHAR(100) NOT NULL,
   is_completed BOOLEAN DEFAULT FALSE,
   FOREIGN KEY (activity_id) REFERENCES Activities(activity_id)
