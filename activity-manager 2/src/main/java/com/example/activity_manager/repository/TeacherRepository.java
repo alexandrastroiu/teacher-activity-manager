940b2c0dccde329c7d57d01a747c447c3c30dc51
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2025-12-17 13:26:26 +0200

    Modify teacher class
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- activity-manager 2/src/main/java/com/example/activity_manager/repository/TeacherRepository.java
+++ activity-manager 2/src/main/java/com/example/activity_manager/repository/TeacherRepository.java
@@ -3,5 +3,8 @@ package com.example.activity_manager.repository;
 import com.example.activity_manager.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+    Optional<Teacher> findByUserUserId(Long userId);
 }
