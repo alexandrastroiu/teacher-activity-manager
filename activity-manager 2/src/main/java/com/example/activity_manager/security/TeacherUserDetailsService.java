f946409ca90276c598846b680d4e54721ffcf35a
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2025-12-25 22:54:44 +0200

    Add service for authentication
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ activity-manager 2/src/main/java/com/example/activity_manager/security/TeacherUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.example.activity_manager.security;
+
+import com.example.activity_manager.model.User;
+import com.example.activity_manager.model.UserRole;
+import com.example.activity_manager.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class
+TeacherUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public TeacherUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User u = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        // Only teachers can log in to UI
+        if (u.getUserRole() != UserRole.TEACHER) {
+            throw new UsernameNotFoundException("Only teachers can log in");
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                u.getUsername(),
+                u.getUserPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_TEACHER"))
+        );
+    }
+}
